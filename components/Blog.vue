9c929cd92f22266a6b757f843ddc164246844087
Javier Pomachagua <javierpp0303@gmail.com>
2020-08-12 12:40:50 -0500

    [Add] Contact Desktop

--- components/Blog.vue
+++ components/Blog.vue
@@ -9,11 +9,17 @@
       >
         Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
       </p>
+      <div class="blog__categories">
+        <h3>UX Design</h3>
+        <h3>Landing Page</h3>
+        <h3>Aplicación Web</h3>
+        <h3>Móvil</h3>
+      </div>
     </div>
     <div class="blog__posts">
       <div v-for="post in posts" :key="post.title" class="blog__post">
         <figure class="blog__post-image">
-          <img :src="post.image" alt="post">
+          <img :src="require(`~/assets/img/${post.image}`)" alt="post">
         </figure>
         <div class="blog__post-description">
           <p>{{ post.date }}</p>
@@ -30,18 +36,33 @@ export default {
     return {
       posts: [
         {
-          image: 'https://via.placeholder.com/400x250',
+          image: 'post.jpg',
           title: 'Post One',
           date: '25 Diciembre 2019'
         },
         {
-          image: 'https://via.placeholder.com/400x250',
+          image: 'post.jpg',
           title: 'Post Two',
           date: '22 Diciembre 2019'
         },
         {
-          image: 'https://via.placeholder.com/400x250',
+          image: 'post.jpg',
           title: 'Post Two',
+          date: '22 Diciembre 2019'
+        },
+        {
+          image: 'post.jpg',
+          title: 'Post Two',
+          date: '22 Diciembre 2019'
+        },
+        {
+          image: 'post.jpg',
+          title: 'Post Two',
+          date: '22 Diciembre 2019'
+        },
+        {
+          image: 'post.jpg',
+          title: 'post Two',
           date: '22 Diciembre 2019'
         }
       ]
@@ -69,13 +90,45 @@ export default {
 .blog__categories h3 {
   @apply w-32 mr-4 mb-2
 }
+.blog__posts {
+  @apply grid grid-cols-1 gap-0
+}
+.blog__post {
+  @apply relative z-10
+}
 .blog__post-description {
-  @apply bg-blue-100-jp py-6
+  @apply hidden bg-blue-100-jp py-6
 }
 .blog__post-description p {
   @apply ml-10 text-white text-xs mb-2
 }
 .blog__post-description h1 {
   @apply ml-10 text-white text-lg
+}
+@screen md {
+  .blog {
+    @apply mt-0
+  }
+  .blog__header {
+    @apply px-56
+  }
+  .blog__title {
+    @apply text-jp-xl
+  }
+  .blog__categories h3 {
+    @apply text-lg
+  }
+  .blog__post:hover .blog__post-description {
+    @apply block absolute bottom-0 w-full
+  }
+  .blog__post:hover{
+    @apply transition delay-150 duration-500 ease-in-out transform scale-105 z-20
+  }
+  .blog__posts {
+    @apply grid-cols-3
+  }
+  .blog__post-image img {
+    @apply h-64 w-full object-cover
+  }
 }
 </style>
--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,14 +1,20 @@
 <template>
   <section class="contactme">
-    <h1 class="contactme__title">
-      Siéntese libre de contactarse conmigo y conversar
-    </h1>
-    <input class="contactme__name" type="text" placeholder="Su nombre">
-    <input class="contactme__email" type="text" placeholder="Su correo">
-    <textarea class="contactme__message" rows="4" placeholder="Escriba su mensaje" />
-    <button class="contactme__cta">
-      Enviar
-    </button>
+    <div class="contactme__form">
+      <form action="#">
+        <h1 class="contactme__title">
+          Siéntese libre de contactarse conmigo y conversar
+        </h1>
+        <input class="contactme__name" type="text" placeholder="Su nombre" required>
+        <input class="contactme__email" type="text" placeholder="Su correo" required>
+        <textarea class="contactme__message" rows="4" placeholder="Escriba su mensaje" required />
+        <button type="submit" class="contactme__cta">
+          Enviar
+        </button>
+      </form>
+    </div>
+    <div class="bg-square-3" />
+    <div class="bg-circle-3" />
   </section>
 </template>
 
@@ -19,22 +25,49 @@ export default {
 </script>
 
 <style>
-.contactme {
-  @apply flex flex-col items-center mb-10
+.contactme__form form {
+  @apply flex flex-col items-center
 }
 .contactme__title {
   @apply mx-10 text-center mb-10 text-base font-bold
 }
 .contactme__name {
-  @apply w-64 border-b-2 border-gray-200-jp mb-6 text-sm text-gray-100-jp outline-none
+  @apply w-96 border-b-2 border-gray-200-jp mb-6 text-sm outline-none
 }
 .contactme__email {
-  @apply w-64 border-b-2 border-gray-200-jp mb-6 text-sm text-gray-100-jp outline-none
+  @apply w-96 border-b-2 border-gray-200-jp mb-6 text-sm outline-none
 }
 .contactme__message {
-  @apply w-64 border-b-2 border-gray-200-jp mb-4 text-sm text-gray-100-jp outline-none
+  @apply w-96 border-b-2 border-gray-200-jp mb-4 text-sm outline-none
 }
 .contactme__cta {
   @apply w-24 mt-6 bg-blue-100-jp text-white rounded-full px-4 py-2
+}
+@screen md {
+  .contactme {
+    @apply relative overflow-hidden;
+  }
+  .contactme__form {
+    @apply border-none m-24 py-10 px-12 bg-white mx-auto shadow-lg;
+    border-radius: 2rem;
+    width: 600px;
+  }
+  .bg-square-3 {
+    @apply absolute bg-blue-jp transform rotate-45;
+    border-radius: 4rem;
+    z-index: -10;
+    width: 40rem;
+    height: 40rem;
+    right: -300px;
+    top: -50px
+  }
+  .bg-circle-3 {
+    @apply absolute bg-blue-jp transform rotate-45 rounded-full;
+    z-index: -10;
+    width: 40rem;
+    height: 40rem;
+    left: -200px;
+    top: -300px
+  }
 }
 </style>

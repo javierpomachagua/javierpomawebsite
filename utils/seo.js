c6d7e5e1706dbf3123a90889b2f1772772f0dd1c
Javier Pomachagua <javierpp0303@gmail.com>
2021-03-30 14:32:25 -0500

    Add twitter card

--- utils/seo.js
+++ utils/seo.js
@@ -13,6 +13,26 @@ export const createSEOMeta = ({ title, description, image, url }) => [
     content: url
   },
   {
+    hid: 'twitter:title',
+    name: 'twitter:title',
+    content: title
+  },
+  {
+    hid: 'twitter:description',
+    name: 'twitter:description',
+    content: description
+  },
+  {
+    hid: 'twitter:image',
+    name: 'twitter:image',
+    content: image
+  },
+  {
+    hid: 'twitter:url',
+    name: 'twitter:url',
+    content: url
+  },
+  {
     hid: 'twitter:card',
     name: 'twitter:card',
     content: 'summary_large_image'

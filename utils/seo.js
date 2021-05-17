6a00f525bddf8ebfe5d577eff7a098b5f576d1ee
Javier Pomachagua <javierpp0303@gmail.com>
2021-05-17 15:57:02 -0500

    fix seo

--- utils/seo.js
+++ utils/seo.js
@@ -1,12 +1,29 @@
 export const createSEOMeta = ({ title, description, image, url }) => [
-  { hid: 'og:title', property: 'og:title', content: title },
-  { hid: 'description', name: 'description', content: description },
+  {
+    hid: 'og:title',
+    property: 'og:title',
+    content: title
+  },
   {
     hid: 'og:description',
     property: 'og:description',
     content: description
   },
-  { hid: 'og:image', property: 'og:image', content: image },
+  {
+    hid: 'og:image',
+    property: 'og:image',
+    content: image
+  },
+  {
+    hid: 'og:image:secure_url',
+    property: 'og:image:secure_url',
+    content: image
+  },
+  {
+    hid: 'og:image:alt',
+    property: 'og:image:alt',
+    content: title
+  },
   {
     hid: 'og:url',
     property: 'og:url',
@@ -18,8 +35,23 @@ export const createSEOMeta = ({ title, description, image, url }) => [
     content: 'summary_large_image'
   },
   {
+    hid: 'twitter:title',
+    name: 'twitter:title',
+    content: title
+  },
+  {
+    hid: 'twitter:description',
+    name: 'twitter:description',
+    content: description
+  },
+  {
     hid: 'twitter:image',
     name: 'twitter:image',
     content: image
+  },
+  {
+    hid: 'twitter:image:alt',
+    name: 'twitter:image:alt',
+    content: title
   }
 ]

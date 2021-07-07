69dd10070e81b9668af8872e4fc4e5b2f5cb0810
Javier Pomachagua <javierpp0303@gmail.com>
2021-07-07 08:59:49 -0500

    add form netlify

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,14 +1,43 @@
 <template>
-  <section class="mx-auto mt-20 contactme md:relative md:overflow-hidden max-w-7xl md:mt-0 md:flex md:justify-center">
-    <div class="relative px-10 md:w-min md:border-none md:m-24 md:py-10 md:px-12 md:bg-white md:shadow-lg md:rounded-2xl">
-      <form class="flex flex-col items-center" action="#">
+  <section
+    class="mx-auto mt-20  contactme md:relative md:overflow-hidden max-w-7xl md:mt-0 md:flex md:justify-center"
+  >
+    <div
+      class="relative px-10  md:w-min md:border-none md:m-24 md:py-10 md:px-12 md:bg-white md:shadow-lg md:rounded-2xl"
+    >
+      <form
+        class="flex flex-col items-center"
+        method="POST"
+        data-netlify="true"
+      >
         <h1 class="mx-10 text-xl font-bold text-center mb-14">
           Siéntese libre de contactarse conmigo y conversar
         </h1>
-        <input class="w-64 mb-10 text-sm border-b-2 outline-none border-gray-200-jp md:w-96" type="text" placeholder="Su nombre" required>
-        <input class="w-64 mb-10 text-sm border-b-2 outline-none border-gray-200-jp md:w-96" type="email" placeholder="Su correo" required>
-        <textarea class="w-64 text-sm border-b-2 outline-none border-gray-200-jp md:w-96" rows="4" placeholder="Escriba su mensaje" required />
-        <button type="submit" class="w-24 px-4 py-2 mt-6 text-white rounded-full bg-blue-100-jp hover:bg-blue-jp">
+        <input
+          class="w-64 mb-10 text-sm border-b-2 outline-none  border-gray-200-jp md:w-96"
+          type="text"
+          placeholder="Su nombre"
+          name="name"
+          required
+        >
+        <input
+          class="w-64 mb-10 text-sm border-b-2 outline-none  border-gray-200-jp md:w-96"
+          type="email"
+          placeholder="Su correo"
+          name="email"
+          required
+        >
+        <textarea
+          class="w-64 text-sm border-b-2 outline-none  border-gray-200-jp md:w-96"
+          rows="4"
+          placeholder="Escriba su mensaje"
+          name="message"
+          required
+        />
+        <button
+          type="submit"
+          class="w-24 px-4 py-2 mt-6 text-white rounded-full  bg-blue-100-jp hover:bg-blue-jp"
+        >
           Enviar
         </button>
       </form>
